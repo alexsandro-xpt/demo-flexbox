35b770c6ccfa3c4fe6c6fcda143c6a6ede3f84e7
Alexsandro <alexsandro_xpt@msn.com>
2019-02-21 15:00:23 -0300

    Initial

---
+++ base.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+  el: '#left',
+  data: {
+    message: 'Hello Vue!',
+    topContent: [
+      { type: 'display1', bgColor: '#383838', width: '300px', src: 'VU.svg' },
+      { type: 'display2', bgColor: '#62cee8', width: '100px', src: 'ONAIR.svg' },
+      { type: 'clock', bgColor: '#62cee8', width: '80px', src: 'RELOGIO.svg' },
+    ],
+    middleContent: [
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+    ],
+    mixerTracks: [
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+      { id: 1 },
+    ]
+  }
+});
